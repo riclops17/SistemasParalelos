9181347c94b9ac7ce830f81fe2a6d34cc70b1fc6
riclops17 <riclops17@gmail.com>
2020-07-06 18:08:09 -0300

    Create Area_BajoCurva_Paralelo.c

---
+++ OpenMP/Area_BajoCurva_Paralelo.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<unistd.h>
+#include<omp.h>
+
+double sampleTime(){
+    struct timespec tv;
+    clock_gettime(CLOCK_MONOTONIC_RAW,&tv);
+    return ((double)tv.tv_sec+((double)tv.tv_nsec)/1000000000.0);
+}
+double funcion(double x){
+      x = x+1;
+     return x;
+ }
+int main(){
+ double t,h,aprox,x;
+ int n, b, i,a;
+ //puntos a y b
+ n = 12500000;
+ a = 1;
+ b = 2000000000;
+ h =(b-a)/n;
+ aprox = (funcion(a)+ funcion(b))/2.0;
+
+  t = sampleTime();
+  //region
+ #pragma omp parallel for private(x) reduction(+:aprox)
+ for(i = 1; i<= n-1 ;i++){
+     x = a+i*h;
+     aprox = aprox + funcion(x);
+ }
+ aprox = h*aprox;
+ t = sampleTime() - t;
+ printf("el area bajo la curva es %f\n",aprox);
+ printf("tiempo transcurrido: %f segundos\n",t);
+ return 0;
+}
